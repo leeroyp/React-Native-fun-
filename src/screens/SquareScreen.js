ed63f2e738c0dff1e9a4ee0def0906e27ba282a9
Leeroy Phili <leeroyphili@gmail.com>
2020-10-05 23:43:43 -0400

    more with props and states

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,20 +1,44 @@
 import React, { useState } from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
-import ColorAdjuster from '../components/ColorAdjuster'
+import { Text, StyleSheet, View} from "react-native";
+import ColorAdjuster from "../components/ColorAdjuster";
 
-const HomeScreen = ({ navigation }) => {
+const COLOR_INCREMENT= 15
+
+const SquareScreen= () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
 
   return (
     <View>
-     <ColorAdjuster color="Red"/>
-     <ColorAdjuster color="Green"/>
-     <ColorAdjuster color="Blue"/>
 
+      <ColorAdjuster
+        onIncrease={() => setRed(red + COLOR_INCREMENT)}
+        onDecrease={() => setRed(red - COLOR_INCREMENT)}
+        color="Red"
+      />
+      <ColorAdjuster
+        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        color="Green"
+      />
+      <ColorAdjuster
+        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        color="Blue"
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red},${green},${blue})`
+        }}
+      />
     </View>
   );
 };
 
-
 const styles = StyleSheet.create({});
 
-export default HomeScreen;
+export default SquareScreen;
