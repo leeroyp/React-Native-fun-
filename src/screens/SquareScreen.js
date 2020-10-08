8d5948a6fe4dbec1d4454f8c157b1238904f4b92
Leeroy Phili <leeroyphili@gmail.com>
2020-10-08 12:59:05 -0400

    More practise with TextInput and states

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -7,21 +7,21 @@ const COLOR_INCREMENT = 15;
 
 const reducer = (state, action ) => {
 // first argument  (state)=== is always the initial state { red:0, green : 0 , blue: 0 }
-// second argument (action)=== is how to change state object { colorToChange: red || green|| blue}
-switch(action.colorToChange){
-    case 'red':
-        return state.red + action.amount > 255 || state.red + action.amount < 0
+// second argument (action)=== is how to change state object { tyoe: change_red || change_green|| change_blue, payload: 15}
+switch(action.type){
+    case 'change_red':
+        return state.red + action.payload > 255 || state.red + action.payload < 0
         ? state
-        :{...state, red: state.red + action.amount};
+        :{...state, red: state.red + action.payload};
         // whatever we return, it will be used in our argument as a new state 
-    case 'green':
-        return state.green + action.amount > 255 || state.green+ action.amount < 0
+    case 'change_green':
+        return state.green + action.payload > 255 || state.green+ action.payload < 0
         ? state
-        :{...state, green: state.green + action.amount};
-    case 'blue':
-        return state.blue  + action.amount > 255 || state.blue + action.amount < 0
+        :{...state, green: state.green + action.payload};
+    case 'change_blue':
+        return state.blue  + action.payload > 255 || state.blue + action.payload < 0
         ?state 
-        : {...state, blue: state.blue + action.amount};
+        : {...state, blue: state.blue + action.payload};
     default:
 
         return state
@@ -36,21 +36,21 @@ const SquareScreen = () => {
     <View>
       <ColorAdjuster
         onIncrease={() => 
-            dispatch({colorToChange: 'red', amount: COLOR_INCREMENT})
+            dispatch({type : 'change_red', payload: COLOR_INCREMENT})
          }
         onDecrease={() => 
-            dispatch({colorToChange: 'red', amount: -1 * COLOR_INCREMENT}) 
+            dispatch({type: 'change_red', payload: -1 * COLOR_INCREMENT}) 
         }
         color="Red"
       />
       <ColorAdjuster
-        onIncrease={() => dispatch({colorToChange: 'green', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({colorToChange: 'green', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({type: 'change_green', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({type: 'change_green', payload: -1 * COLOR_INCREMENT})}
         color="Green"
       />
       <ColorAdjuster
-        onIncrease={() => dispatch({colorToChange: 'blue', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({colorToChange: 'blue', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({type: 'change_blue', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({type: 'change_blue', payload: -1 * COLOR_INCREMENT})}
         color="Blue"
       />
       <View
